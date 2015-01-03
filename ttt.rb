7ecfdacf4674debe37f6fb2ab0eb0d5e2105e533
Rick Peyton <peytorb@gmail.com>
2015-01-03 05:40:19 -0600

    Working tic tac toe with dumb computer

--- ttt.rb
+++ ttt.rb
@@ -12,10 +12,6 @@ VICTORY_CONDITIONS =
     g: '159',
     h: '357' }
 
-possible_moves =
-  { '1' => ' ', '2' => ' ', '3' => ' ', '4' => ' ', '5' => ' ',
-    '6' => ' ', '7' => ' ', '8' => ' ', '9' => ' ' }
-
 def validate_pick_position?(possible_moves, pick)
   if possible_moves[pick] == ' '
     true
@@ -24,16 +20,12 @@ def validate_pick_position?(possible_moves, pick)
   end
 end
 
-def victory_check(picks, possible_wins)
-  possible_wins.values.each do |v|
-    three_to_win = 0
-    picks.each_char do |c|
-      if v.includes?(c)
-        three_to_win += 1
-      end
-      if three_to_win == 3
-        return true
-      end
+def check_for_victory(pick, all_picks, victory_conditions)
+  victory_conditions.values.each do |v|
+    if v.include?(pick)
+      victory_counter = 0
+      all_picks.each { |p| victory_counter += 1 if v.include? p }
+      return true if victory_counter == 3
     end
   end
   return false
@@ -57,7 +49,8 @@ puts <<EOF
 EOF
 end
 
-all_player_picks = []
+player_score = 0
+computer_score = 0
 
 puts '###########################'
 puts '# Welcome to Tic Tac Toe! #'
@@ -65,32 +58,63 @@ puts '###########################'
 puts ''
 
 begin
+  all_player_picks = []
+  all_comp_picks = []
+  possible_moves =
+    { '1' => ' ', '2' => ' ', '3' => ' ', '4' => ' ', '5' => ' ',
+      '6' => ' ', '7' => ' ', '8' => ' ', '9' => ' ' }
+  total_moves = 0
   draw_game_board(possible_moves)
-
+  game_over = false
   begin
-    puts 'Pick a play on the game board (1 - 9)'
-    player_pick = gets.chomp.downcase
-    validate_pick = validate_pick_position?(possible_moves, player_pick)
-  end while validate_pick == false
 
-  possible_moves[player_pick] = 'X'
-  all_player_picks << player_pick
-  draw_game_board(possible_moves)
+    begin
+      puts 'Pick a play on the game board (1 - 9)'
+      player_pick = gets.chomp.downcase
+      validate_pick = validate_pick_position?(possible_moves, player_pick)
+    end while validate_pick == false
+    total_moves += 1
 
-  if victory_check(all_player_picks, VICTORY_CONDITIONS)
-    puts 'You win!'
-    break
-  end
+    possible_moves[player_pick] = 'X'
+    all_player_picks << player_pick
+    draw_game_board(possible_moves)
 
-  # Computer Turn
-  # if a victory condition exists move there
-  # else if a player victory exists move there
-  # else pick random
+    if check_for_victory(player_pick, all_player_picks, VICTORY_CONDITIONS)
+      puts 'You Win!'
+      player_score += 1
+      puts "Wins: #{player_score}. Computer Wins: #{computer_score}"
+      game_over = true
+    end
 
-  # Repeat until there is a victor
+    unless game_over || total_moves == 9
+      begin
+        computer_pick = rand(1..9).to_s
+        validate_pick = validate_pick_position?(possible_moves, computer_pick)
+      end while validate_pick == false
+      total_moves += 1
 
-  # Increment score for victor
+      possible_moves[computer_pick] = 'O'
+      all_comp_picks << computer_pick
+      puts 'Computer makes its move...'
+      draw_game_board(possible_moves)
 
-  puts "Would you like to play again? (y/n)"
+      if check_for_victory(computer_pick, all_comp_picks, VICTORY_CONDITIONS)
+        puts 'Computer Wins!'
+        computer_score += 1
+        puts "Wins: #{player_score}. Computer Wins: #{computer_score}"
+        game_over = true
+      end
+    end
+
+    if total_moves == 9 && game_over == false
+      puts 'It\'s a draw!'
+      game_over = true
+      puts "Wins: #{player_score}. Computer Wins: #{computer_score}"
+    end
+
+  end while game_over == false
+
+  puts 'Would you like to play again? (y/n)'
   keep_playing = gets.chomp.downcase
+
 end while keep_playing == 'y'
